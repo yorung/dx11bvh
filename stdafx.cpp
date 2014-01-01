f1c2c4c82437523459398853974ecbc153e8e7a3
yorung <yorung@gmail.com>
2014-01-01 12:57:12 +0900

    files from 8fea80efcba335f2e50da994bbed1599bf155d2b

--- stdafx.cpp
+++ stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// dx11bvh.pch will be the pre-compiled header
+// dx11x.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
