3a20a09fbfa28a96b7e8a6056b057db42e52c54b
yorung <yorung@gmail.com>
2014-01-25 19:16:50 +0900

    refactoring

--- source/app.cpp
+++ source/app.cpp
@@ -207,8 +207,8 @@ void App::Update()
 
 void App::Draw()
 {
-//	double time = GetTime() - startTime;
-	double time = 0;
+	double time = GetTime() - startTime;
+//	double time = 0;
 
 //	XMMATRIX mRot = XMMatrixRotationQuaternion(XMQuaternionRotationAxis(XMVectorSet(1, 0, 0, 0), time / 2 * XM_PI));
 	XMMATRIX mRot = XMMatrixRotationQuaternion(quat);
@@ -230,8 +230,8 @@ void App::Draw()
 			Bvh* bvh = dynamic_cast<Bvh*>(it);
 
 			if (bvh) {
-			//	meshTiny->DrawBvh(bvh, time);
-				meshTiny->Draw(animationNumber, time);
+				meshTiny->DrawBvh(bvh, time);
+			//	meshTiny->Draw(animationNumber, time);
 				if (GetKeyState('T') & 0x01) {
 					DrawBoneNames(bvh);
 				}
--- source/mesh_x.cpp
+++ source/mesh_x.cpp
@@ -1279,17 +1279,11 @@ void MeshX::ApplyBvhInitialStance(const Bvh* bvh)
 //	const char* xBoneNames[] = {"Bip01_R_UpperArm", "Bip01_L_UpperArm", "Bip01_L_Thigh", "Bip01_R_Thigh" };
 	const char* xBoneNames[] = {
 		"Bip01_L_Clavicle", "Bip01_R_Clavicle",
-		"Bip01_R_UpperArm",
-		"Bip01_L_UpperArm",
-		"Bip01_L_Forearm",
-		"Bip01_R_Forearm",
-		"Bip01_L_Thigh",
-		"Bip01_R_Thigh",
-		"Bip01_R_Calf",
-		"Bip01_L_Calf",
-		"Bip01_R_Calf",
-		"Bip01_L_Foot",
-		"Bip01_R_Foot",
+		"Bip01_R_UpperArm", "Bip01_L_UpperArm",
+		"Bip01_L_Forearm", "Bip01_R_Forearm",
+		"Bip01_L_Thigh", "Bip01_R_Thigh",
+		"Bip01_R_Calf", "Bip01_L_Calf",
+		"Bip01_L_Foot", "Bip01_R_Foot",
 	};
 //	const char* xBoneNames[] = {"Bip01_R_UpperArm" };
 //	const char* xBoneNames[] = {"Bip01_L_Calf", "Bip01_R_Calf", "Bip01_R_UpperArm", "Bip01_L_UpperArm",  };
@@ -1328,8 +1322,6 @@ void MeshX::ApplyBvhInitialStance(const Bvh* bvh)
 		XMVECTOR world100 = XMVector3Normalize(bvhChild.result.Translation() - bvhF.result.Translation());
 		XMVECTOR worldBone = XMVector3Normalize(xChild.result.Translation() - f->result.Translation());
 
-//		XMVECTOR world100 = XMVector3Normalize(XMLoadFloat4x4(&f->result).r[0]);
-//		XMVECTOR worldBone = XMVector3Normalize(bvhF.offset.Translation());
 		XMVECTOR rotAxis = XMVector3Cross(worldBone, world100);
 		float rotRad = acosf(XMVectorGetX(XMVector3Dot(worldBone, world100)));
 
