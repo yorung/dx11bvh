67989013ee5a9becd49c9860bd2d2335a258150a
yorung <yorung@gmail.com>
2015-03-02 01:35:01 +0900

    test to avoid using macro to get array size

--- source/helper.h
+++ source/helper.h
@@ -1,10 +1,15 @@
-#define dimof(x) (sizeof(x) / sizeof(x[0]))
+//#define dimof(x) (sizeof(x) / sizeof(x[0]))
 
 void *LoadFile(const char *fileName, int* size = nullptr);
 bool SaveFile(const char *fileName, const uint8_t* buf, int size);
 void GoMyDir();
 double GetTime();
 float Random();
+
+template <class T, size_t N> inline size_t dimof(T(&)[N])
+{
+	return N;
+}
 
 template <class T> inline void SAFE_DELETE(T& p)
 {
