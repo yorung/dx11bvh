d939dff6865f2dc2de34d97bd4f85c011c90c6d6
yorung <yorung@gmail.com>
2015-08-17 23:02:04 +0900

    load texture using GDI+

--- source/tex_man11.cpp
+++ source/tex_man11.cpp
@@ -2,6 +2,53 @@
 
 TexMan11 texMan;
 
+namespace Gdiplus {
+	using std::min;
+	using std::max;
+}
+#include <gdiplus.h>
+#pragma comment(lib, "gdiplus.lib")
+
+static ID3D11ShaderResourceView* LoadTextureViaOS(const char* name)
+{
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiplusToken;
+	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
+	WCHAR wc[MAX_PATH];
+	MultiByteToWideChar(CP_ACP, 0, name, -1, wc, dimof(wc));
+	Gdiplus::Bitmap* image = new Gdiplus::Bitmap(wc);
+
+	int w = (int)image->GetWidth();
+	int h = (int)image->GetHeight();
+	Gdiplus::Rect rc(0, 0, w, h);
+
+	Gdiplus::BitmapData* bitmapData = new Gdiplus::BitmapData;
+	image->LockBits(&rc, Gdiplus::ImageLockModeRead, PixelFormat32bppARGB, bitmapData);
+
+	std::vector<uint32_t> col;
+	col.resize(w * h);
+	for (int y = 0; y < h; y++) {
+		memcpy(&col[y * w], (char*)bitmapData->Scan0 + bitmapData->Stride * y, w * 4);
+		for (int x = 0; x < w; x++) {
+			uint32_t& c = col[y * w + x];
+			c = (c & 0xff00ff00) | ((c & 0xff) << 16) | ((c & 0xff0000) >> 16);
+		}
+	}
+	image->UnlockBits(bitmapData);
+	delete bitmapData;
+	delete image;
+	Gdiplus::GdiplusShutdown(gdiplusToken);
+
+	CD3D11_TEXTURE2D_DESC desc(DXGI_FORMAT_R8G8B8A8_UNORM, w, h, 1, 1, D3D11_BIND_SHADER_RESOURCE);
+	D3D11_SUBRESOURCE_DATA r = { &col[0], (uint32_t)w * 4, 0 };
+	ID3D11Texture2D* tex = nullptr;
+	ID3D11ShaderResourceView* srv = nullptr;
+	deviceMan11.GetDevice()->CreateTexture2D(&desc, &r, &tex);
+	deviceMan11.GetDevice()->CreateShaderResourceView(tex, nullptr, &srv);
+	SAFE_RELEASE(tex);
+	return srv;
+}
+
 TexMan11::TexMan11()
 {
 	texs.push_back(nullptr);	// make ID 0 invalid
@@ -22,13 +69,13 @@ TexMan11::TMID TexMan11::Create(const char *name)
 
 	ID3D11ShaderResourceView *tex = nullptr;
 
-	WCHAR wname[MAX_PATH];
-	MultiByteToWideChar(CP_ACP, 0, name, -1, wname, dimof(wname));
-
 	if (!_stricmp(".dds", name + strlen(name) - 4)) {
+		WCHAR wname[MAX_PATH];
+		MultiByteToWideChar(CP_ACP, 0, name, -1, wname, dimof(wname));
 		CreateDDSTextureFromFileEx(deviceMan11.GetDevice(), wname, 0, D3D11_USAGE_DEFAULT, D3D11_BIND_SHADER_RESOURCE, 0, 0, false, nullptr, &tex);
 	} else {
-		CreateWICTextureFromFileEx(deviceMan11.GetDevice(), deviceMan11.GetContext(), wname, 0, D3D11_USAGE_DEFAULT, D3D11_BIND_SHADER_RESOURCE, 0, 0, false, nullptr, &tex);
+		tex = LoadTextureViaOS(name);
+	//	CreateWICTextureFromFileEx(deviceMan11.GetDevice(), deviceMan11.GetContext(), wname, 0, D3D11_USAGE_DEFAULT, D3D11_BIND_SHADER_RESOURCE, 0, 0, false, nullptr, &tex);
 	}
 	if (!tex) {
 		return INVALID_TMID;
@@ -39,7 +86,7 @@ TexMan11::TMID TexMan11::Create(const char *name)
 
 static ID3D11ShaderResourceView* CreateWhiteTexture()
 {
-	CD3D11_TEXTURE2D_DESC desc(DXGI_FORMAT_R8G8B8A8_UNORM, 1, 1);
+	CD3D11_TEXTURE2D_DESC desc(DXGI_FORMAT_R8G8B8A8_UNORM, 1, 1, 1, 1, D3D11_BIND_SHADER_RESOURCE);
 	uint32_t white = 0xffffffff;
 	D3D11_SUBRESOURCE_DATA r = { &white, 4, 4 };
 	ID3D11Texture2D* tex = nullptr;
